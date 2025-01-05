bf3faadc752c608fe32b851f003fae3069a1719b
h-kanta <uta_082812@icloud.com>
2025-01-05 12:24:29 +0900

    Initial Commit

---
+++ ShoppingManagementApp/ShoppingManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingManagementAppApp.swift
+//  ShoppingManagementApp
+//
+//  Created by 堀川貫太 on 2025/01/05.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShoppingManagementAppTests/ShoppingManagementAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingManagementAppTests.swift
+//  ShoppingManagementAppTests
+//
+//  Created by 堀川貫太 on 2025/01/05.
+//
+
+import Testing
+@testable import ShoppingManagementApp
+
+struct ShoppingManagementAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
